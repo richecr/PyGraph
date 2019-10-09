232e3948a693a44ce7563ad1a0738aea1571ff19
Gabriel Vassoler <gabiv98@gmail.com>
2019-10-08 22:02:28 -0300

    Ajuste da função has_loop
    A função anterior não estava funcionando, então fiz os ajustes que achei cabíveis. A princípio está funcionando. Escreverei o caso de teste em breve para submissão.

--- py_graph_t/SimpleGraph.py
+++ py_graph_t/SimpleGraph.py
@@ -279,27 +279,33 @@ class SimpleGraph:
         return edges
     
 
-    def has_loop(self, graph):
+    def has_cycle(self,v,visited,parent):   
+        visited[v]= True
+  
+        for i in self.vertices: 
+            if(self.vertices_adjacency(v,i)):
+                if  visited[i] == False :  
+                    if(self.has_cycle(i, visited, v)): 
+                        return True
+                elif  parent != i: 
+                    return True
+          
+        return False
+
+
+    def has_loop(self):  
         """
-        Método retorna True se gráfo tem loop/cycle, False caso nao tenha.
-        Parâmetros:
-        ----------
-        graph: *
-            - Object graph of SimpleGraph()
+        Função retorna True se gráfo tem loop/cycle, False caso nao tenha.
         """
-        visited = []
-        done = False
-        for i in graph.edges:
-            a = i.vertex_a.value
-            b = i.vertex_b.value
-            if (a,b) not in visited:
-                if (b,a) not in visited:
-                    visited.append((i.vertex_a.value,i.vertex_b.value))
-                else:
-                    done = True
-                    break
-        if done:
-            return True
+        visited = dict()
+        for i in self.vertices:
+            visited[i] = False
+
+        for i in self.vertices: 
+            if visited[i] == False: 
+                if(self.has_cycle(i, visited, -1)) == True: 
+                    return True
+          
         return False
         
     def __str__(self):
