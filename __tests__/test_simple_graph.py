a34d0af862db5b8477e04e81ce6d3bcdeac51261
Gabriel Vassoler <gabiv98@gmail.com>
2019-10-09 09:48:47 -0300

    Criação de dois casos de teste para a função has_loop()

--- __tests__/test_simple_graph.py
+++ __tests__/test_simple_graph.py
@@ -89,3 +89,26 @@ class TestSimpleGraph:
         self.graph.add_vertex("b")
         self.graph.add_edge("a", "b", "x")
         assert str(self.graph.show_edge('a', 'b')) == str('x: Vértice a -> Vértice b')
+        
+    def test_true_loop_graph(self):
+        self.graph.edges = []
+        self.graph.vertices = dict()
+        self.graph.add_vertex("a")
+        self.graph.add_vertex("b")
+        self.graph.add_vertex("c")
+        self.graph.add_edge("a", "b", 1)
+        self.graph.add_edge("b", "c", 2)
+        self.graph.add_edge("c", "a", 3)
+        assert self.graph.has_loop() == True
+
+    def test_false_loop_graph(self):
+        self.graph.edges = []
+        self.graph.vertices = dict()
+        self.graph.add_vertex("a")
+        self.graph.add_vertex("b")
+        self.graph.add_vertex("c")
+        self.graph.add_vertex("d")
+        self.graph.add_edge("a", "b", 1)
+        self.graph.add_edge("b", "c", 2)
+        self.graph.add_edge("c", "d", 3)
+        assert self.graph.has_loop() == False
