6571474d588a9952f0c26abbd628b6c38ae34787
mateustranquilino <mateus.tranquilino@ccc.ufcg.edu.br>
2019-10-26 23:20:12 -0300

    Método para retornar matriz de adjacência para um grafo não ponderado. Restrição de arestas paralelas removidas da classe Graph.

--- Main.py
+++ Main.py
@@ -13,3 +13,23 @@ g.add_edge("a", "a", name="d")
 i = g.incidence_list()
 print(i)
 print(g.__str__())
+'''
+graph = Graph()
+graph.add_vertex("a")
+graph.add_vertex("b")
+graph.add_vertex("c")
+graph.add_vertex("d")
+graph.add_vertex("e")
+
+graph.add_edge("a", "b", name="s")
+graph.add_edge("a", "a", name="t")
+graph.add_edge("b", "c", name="u")
+graph.add_edge("c", "d", name="v")
+graph.add_edge("c", "d", name="w")
+graph.add_edge("d", "b", name="x")
+graph.add_edge("a", "d", name="y")
+graph.add_edge("e", "d", name="z")
+adjacency_matrix =  graph.adjacency_matrix()
+for line in adjacency_matrix:
+    print(line, adjacency_matrix[line])
+'''
