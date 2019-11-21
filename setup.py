bae3b3ba60eb7bd091a6a88e4527cc165a7e015c
Rich <richelton14@gmail.com>
2019-11-21 08:41:08 -0300

    Corrigindo a versão de lançamento

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='PyGraphT',
-    version='0.1.4',
+    version='0.1.0',
     author='Rich Elton',
     author_email='richelton14@gmail.com',
     description='Biblioteca com intuito de implementar os grafos e seus \
